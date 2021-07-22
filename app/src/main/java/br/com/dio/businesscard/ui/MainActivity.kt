16be968ac6578e47f38a50bd40a4f0d9747e65b5
caaarllosr <carlosrvmelo@gmail.com>
2021-07-22 16:26:21 -0300

    configurando Room Database - criando o ViewModel

--- app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -3,14 +3,24 @@ package br.com.dio.businesscard.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import br.com.dio.businesscard.App
 import br.com.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    private val adapter by lazy { BusinessCardAdapter() }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListener()
     }
 
@@ -19,5 +29,11 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, { businessCards ->
+            adapter.submitList(businessCards)
+        })
     }
 }
